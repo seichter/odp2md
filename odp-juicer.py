53dd9bd1fdd5963005c15056294ff95a41e0d9c8
Hartmut Seichter <hartmut@technotecture.com>
2020-01-08 14:01:30 +0100

    WiP

--- odp-juicer.py
+++ odp-juicer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
- ODP Juicer is a tool to convert LibreOffice Impress Presentations in ODP format to Markdown compatible with pandoc
+ ODP Juicer is a tool to convert LibreOffice Impress Presentations in ODP format 
+ to Pandoc Markdown
  """
 
 import os
@@ -13,27 +14,53 @@ import xml.dom.minidom as dom
 class Slide:
     def __init__(self):
         self.name = 'Unknown'
-        self.text = []
+        self.text = [[]]
         self.notes = []
         self.media = []
-        self.data = [{'p' : 'dadadad'},{'l' : ['blah', 'more']  }]
+        #self.data = ['blah', ['more','blah blah']]
+
+    def generateMarkdownBody(self,d,depth):
+        out = ""
+        for v in d:
+            if isinstance(v,list):
+                out += self.generateMarkdownBody(v,depth+1)
+            else:
+                out += "\t" * depth
+                out += "- {0}\n".format(v)
+        return out
+
+    def generateMarkdown(self):
+        
+        out = "## {0}\n".format(self.name)
+
+        out += self.generateMarkdownBody(self.text,0)
+        
+        return out
 
     def __str__(self):
-        return " ".join([self.name," ".join(self.text)])
+        return self.generateMarkdown()
 
 class ODPJuicer:
     def __init__(self):
         self.slides = []
         self.doc = None
         self.currentSlide = None
-
-    def debug(self,node):
-        print("node ", node)
+        self.currentText = []
 
     def getTextFromNode(self,node):
-        for c in node.childNodes:
-            if c.nodeType == c.TEXT_NODE:
-                self.currentSlide.text + c.data
+        
+        if node.nodeType == node.TEXT_NODE:
+            self.currentText.append(node.data)
+            # self.currentSlide.text + node.data
+            # print('text: ',node.data,node.parentNode.tagName)
+            # for k,v in node.parentNode.attributes.items():
+                # print("\t{0}:{1}".format(k,v))
+
+
+    # def getTextFromNode(self,node):
+    #     for c in node.childNodes:
+    #         if c.nodeType == c.TEXT_NODE:
+    #             self.currentSlide.text + c.data
 
     def hasAttributeWithValue(self,node,name,value):
         for attribute_name,attribute_value in node.attributes.items():
@@ -41,32 +68,39 @@ class ODPJuicer:
                 return True
         return False
 
-    def handleTextSpan(self,node):
-        self.getTextFromNode(node)
-        pass
+    def debugNode(self,node):
+        print('node ', node.tagName)
 
-    def handleTextParagraph(self,node):
-        # iterate over textspans
-        self.getTextFromNode(node)
+    # def handleTextSpan(self,node):
+    #     self.getTextFromNode(node)
+    #     pass
 
-    def handleTextListItem(self,node):
-        for c in node.childNodes:
-            if c.tagName == 'text:p':
-                self.handleTextParagraph(c)
-            if c.tagName == 'text:list':
-                self.handleTextList(c)
+    # def handleTextParagraph(self,node):
+    #     # iterate over textspans
+    #     self.getTextFromNode(node)
+
+    # def handleTextListItem(self,node):
+    #     for c in node.childNodes:
+    #         self.debugNode(c)
+    #         if c.tagName == 'text:p':
+    #             self.handleTextParagraph(c)
+    #         elif c.tagName == 'text:list':
+    #             self.handleTextList(c)
     
-    def handleTextBox(self,node):
-        for c in node.childNodes:
-            if c.tagName == 'text:p':
-                self.handleTextParagraph(c)
-            if c.tagName == 'text:list':
-                self.handleTextList(c)
+    # def handleTextBox(self,node):
+    #     for c in node.childNodes:
+    #         if c.tagName == 'text:p':
+    #             self.handleTextParagraph(c)
+    #         if c.tagName == 'text:list':
+    #             self.handleTextList(c)
 
-    def handleTextList(self,node):
-        for c in node.childNodes:
-            if c.tagName == 'text:list-item':
-                self.handleTextListItem(c)
+    # def handleTextList(self,node):
+    #     self.currentSlide.text.append([])
+    #     print(self.currentSlide.text)
+
+        # for c in node.childNodes:
+        #     if c.tagName == 'text:list-item':
+        #         self.handleTextListItem(c)
 
     def handleFrame(self,node):
         for c in node.childNodes:
@@ -87,11 +121,17 @@ class ODPJuicer:
         pass
 
     def handleNode(self,node):
+    
+
+        # if node.attributes['draw:name']:
+            # print('Name!')
+        self.getTextFromNode(node)
 
         tag_functions = {
                 #'draw:text-box' : self.handleTextBox,
-                'draw:page' : self.handlePage,
-                'text:p' : self.getTextFromNode
+                # 'text:list' : self.handleTextList,
+                'draw:page' : self.handlePage
+                # 'text:p' : self.getTextFromNode
                 }
 
         for c in node.childNodes:
@@ -100,7 +140,8 @@ class ODPJuicer:
                 print('handled',c.tagName)
             except:
                 try:
-                    print('not handled:',c.tagName)
+                    # print('not handled:',c.tagName)\
+                    pass
                 except:
                     self.currentSlide.text.append(c.data)
 
@@ -115,8 +156,8 @@ class ODPJuicer:
             self.slides.append(self.currentSlide)
 
         # debug
-        for slide in self.slides:
-            print(slide)
+        # for slide in self.slides:
+        #     print(slide)
 
     def open(self,fname):
         with zipfile.ZipFile(fname) as odp:
@@ -128,6 +169,7 @@ class ODPJuicer:
                         doc = dom.parseString(index.read())
                         self.handleDocument(doc)
 
+
 def main():
     parser = argparse.ArgumentParser(description='OpenDocument Presentation Parser')
     parser.add_argument("--input", default="",
@@ -137,6 +179,8 @@ def main():
 
     juicer = ODPJuicer()
     juicer.open(args.input)
+
+
 
 if __name__ == '__main__':
     main()
