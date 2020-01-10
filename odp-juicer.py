50e04c5979962b9ded5eb0341dc214d4c233d9da
Hartmut Seichter <hartmut@technotecture.com>
2020-01-10 08:19:11 +0100

    add license and copyright notice

--- odp-juicer.py
+++ odp-juicer.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
 """
- ODP Juicer is a tool to convert LibreOffice Impress Presentations in ODP format 
- to Pandoc Markdown
+
+ODP Juicer 2020.1.0
+
+ODP Juicer is a tiny tool to convert 
+OpenDocument formatted presentations (ODP) 
+to Pandocs' Markdown.
+
+(c) Copyright 2019-2020 Hartmut Seichter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+Usage:
+
+$> python odp-juicer --input <myslide.odp>
+
  """
 
 import os
@@ -15,7 +41,7 @@ import xml.dom.minidom as dom
 
 class Slide:
     def __init__(self):
-        self.name = 'Unknown'
+        self.title = 'Unknown'
         self.text = [[]]
         self.notes = []
         self.media = []
@@ -64,6 +90,8 @@ class ODPJuicer:
         return None
 
     def hasAttributeWithValue(self,node,name,value):
+        if node.attributes == None:
+            return False
         for attribute_name,attribute_value in node.attributes.items():
             if attribute_name == name and attribute_value == value:
                 return True
@@ -88,13 +116,17 @@ class ODPJuicer:
         elif self.hasAttributeWithValue(node,"presentation:class","outline"):
             self.currentScope = Scope.OUTLINE
         else:
-            print("Unhandled Scope!")
-
+            pass
+            # print("Unhandled Scope!")
 
         t = self.getTextFromNode(node)
 
         if t != None:
-            self.currentText += (" " * self.currentDepth) + t
+            if self.currentScope == Scope.OUTLINE:
+                self.currentText += (" " * self.currentDepth) + t
+            elif self.currentScope == Scope.TITLE:
+                self.currentSlide.title += self.currentText
+            
 
         for c in node.childNodes:
             self.currentDepth = self.currentDepth + 1
